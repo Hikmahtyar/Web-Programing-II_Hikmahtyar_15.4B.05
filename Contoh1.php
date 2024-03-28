772f8a4d826a1d29c8dad3a830179776a198adda
Hikmahtyar <hikmahtyar07@gmail.com>
2024-03-28 14:48:14 +0700

    Wp2

--- Contoh1.php
+++ Contoh1.php
@@ -4,9 +4,7 @@ class Contoh1 extends CI_Controller
     public function index() 
     { 
         echo "<h1>Perkenalkan</h1>"; 
-        echo"Nama saya Imam Nawawi 
-        Saya tingga di daerah Ciputat 
-        olah raga yang saya sukai adalah 
-        Bulutangkis"; 
+        echo"Nama saya Hikmahtyar Nurhaqiqi Saya tinggal dibekasi
+        Hobi saya bermain catur"; 
     } 
 }
